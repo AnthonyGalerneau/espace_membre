e36b486c742c14ce4088d9f6b44630f627094ba4
Anthony Galerneau <contact@anthony-galerneau.fr>
2018-08-11 00:33:05 +0200

    mise en page d'inscription.php

--- inscription.php
+++ inscription.php
@@ -54,21 +54,28 @@ if(isset($_POST['pseudo']) && isset($_POST['email']) && isset($_POST['pass']) &&
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
 	<title>inscription</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 	<form action="" method="post">
+		<legend>Inscrivez-vous :</legend>
 	    <label for="pseudo">Votre identifiant</label>
 	    <input type="text" name="pseudo" id="pseudo">
+	    <br>
 
 	    <label for="email">Votre adresse e-mail</label>
 	    <input type="text" name="email" id="email">
+	    <br>
 
 	    <label for="pass">Votre mot de passe</label>
 	    <input type="password" name="pass" id="pass">
+	    <br>
 
 	    <label for="pass_confirm">Confirmez votre mot de passe</label>
 	    <input type="password" name="pass_confirm" id="pass_confirm">
+	    <br>
 
 	    <button type="submit">S'inscrire</button>
 	</form>
